86d83a629c7ebaef6dcac4aca256c4c56325bc58
Liam Oscaris <hwu126@usask.ca>
2025-11-01 12:02:14 -0600

    add square space and moving agents

---
+++ ABMMaskEffectiveness/_alp/Agents/Main/Code/Events.java
@@ -0,0 +1,10 @@
+void updateNetworkEvent()
+{/*ALCODESTART::1762017198541*/
+applyNetwork();
+/*ALCODEEND*/}
+
+void initialInfectionEvent()
+{/*ALCODESTART::1762017198552*/
+sendToRandomAgentInside("Infect");
+/*ALCODEEND*/}
+
