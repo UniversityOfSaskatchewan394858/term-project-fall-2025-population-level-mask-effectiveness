8002c451ec7314a38b77512dee3d5944ebbf4b22
Runn077 <rupu0777@gmail.com>
2025-11-23 16:09:23 -0600

    forgot to press save on last commit

--- ABMMaskEffectiveness/_alp/Agents/Person/Code/Events.java
+++ ABMMaskEffectiveness/_alp/Agents/Person/Code/Events.java
@@ -5,7 +5,38 @@ void updateFearLevel()
 //System.out.println("Infective%: " + ((double) main.People.Infective() / main.startingPopulation));
 //System.out.println("Dead: " + ((double) main.People.Dead() / main.startingPopulation));
 
-fearLevel = ((double) main.People.Infective() / (main.startingPopulation) + ((double) main.People.Dead() / (main.startingPopulation)));
+//fearLevel = ((double) main.People.Infective() / (main.startingPopulation) + ((double) main.People.Dead() / (main.startingPopulation)));
+
+double fearRadius = main.spaceSideLength * 0.1; // radius of 10% of the box
+
+// Get list of neighbors
+List<Person> neighbors = getNeighbors(fearRadius);
+
+// Count how many neighbors are infective
+int nearbyInfective = 0;
+for (Person p : neighbors) {
+    if (p.isInfective()) {
+        nearbyInfective++;
+    }
+}
+
+// Count how many neighbors are dead
+int nearbyDead = 0;
+for (Person p : neighbors) {
+    if (p.isDead()) {
+        nearbyDead++;
+    }
+}
+
+// Get the total number of neighbors
+int neighborhoodSize = neighbors.size();
+
+// Calculate the fear level for this agent
+if (neighborhoodSize > 0) {
+    fearLevel = (((double) nearbyInfective / neighborhoodSize) + ((double) nearbyDead / neighborhoodSize));
+} else {
+    fearLevel = 0; // No neighbors no fear
+}
 /*ALCODEEND*/}
 
 void updateVaccinationRate()
