57656cd5e65f9e834b967bfe92e6da05beacac2e
Liam Oscaris <hwu126@usask.ca>
2025-11-28 21:35:23 -0600

    apply ABMMaskEffectivenessDEV changes to ABMMaskEffectivenessV2

--- ABMMaskEffectivenessV2/_alp/Agents/Main/Code/Events.java
+++ ABMMaskEffectivenessV2/_alp/Agents/Main/Code/Events.java
@@ -17,6 +17,80 @@ traceln("Infected surpassed susceptibles on day: " + time());
 
 void updateMaskPopularity()
 {/*ALCODESTART::1762217525125*/
-maskPopularity = (double) People.Masked() / (startingPopulation - People.Dead())
+maskPopularity = (double) People.Masked() / (startingPopulation - deadAgentCount)
+/*ALCODEEND*/}
+
+void updateDataSets()
+{/*ALCODESTART::1764298439539*/
+int day = (int) Math.floor(time());
+int prevDay;
+
+// Updates datasets with previous day's values in the hashmaps
+if (day > 0) {
+	prevDay = day - 1;
+	
+	if (incidentInfectionCount.containsKey(prevDay)) {
+		incidentInfectionsDS.add(prevDay, 
+		incidentInfectionCount.get(prevDay));
+	}
+	
+	if (maskingPreventedPotentialExposureCount.containsKey(prevDay)) {
+		maskingPreventedPotentialExposureCountDS.add(prevDay, 
+		maskingPreventedPotentialExposureCount.get(prevDay));
+	}
+	
+	if (vaccinePreventedPotentialExposureCount.containsKey(prevDay)) {
+		vaccinePreventedPotentialExposureCountDS.add(prevDay,
+		vaccinePreventedPotentialExposureCount.get(prevDay));
+	}
+}
+/*ALCODEEND*/}
+
+void outputWriter()
+{/*ALCODESTART::1764380390480*/
+// Writing dailyTimeseries.csv
+dailyTimeseriesOutput.println("Day," + 
+	"Count of Incident Infections," + 
+	"Count of Potential Exposures Prevented by Masking," +
+	"Susceptible," +
+	"Exposed," +
+	"Infective," + 
+	"Recovered," +
+	"Vaccinated," +
+	"VaccinatedRecovered," +
+	"Dead");
+for (int day = 0; day <= 365; day++) {
+	int incidentInfections = incidentInfectionCount.getOrDefault(day, 0);
+	int preventedExposures = maskingPreventedPotentialExposureCount.getOrDefault(day, 0);
+	int[] stateCounts = dailyStateCounts.get(day);
+	
+	// Slicing string and getting rid of [ and ]"
+	String stateCountsStr = Arrays.toString(stateCounts);
+	stateCountsStr = stateCountsStr.substring(1, stateCountsStr.length()-1).replaceAll("\\s+", "");
+	dailyTimeseriesOutput.println(day + "," +
+		incidentInfections + "," +
+		preventedExposures + "," +
+		stateCountsStr
+	);
+}
+
+// Writing infectionChains.csv
+infectionChainsOutput.println("Time,Susceptible ID,Susceptible Masking Status,Infective ID,Infective Masking Status");
+for (String chain : infectionChains) {
+	infectionChainsOutput.println(chain);
+}
+
+// Closing each file
+dailyTimeseriesOutput.close();
+infectionChainsOutput.close();
+
+traceln("Model has finished running. Output files can be found in /output/");
+/*ALCODEEND*/}
+
+void updateDailyStateCounts()
+{/*ALCODESTART::1764382390757*/
+int day = (int) Math.floor(time());
+int[] stateCounts = {People.Susceptible(), People.Exposed(), People.Infective(), People.Recovered(), People.Vaccinated(), People.VaccinatedRecovered(), deadAgentCount};
+dailyStateCounts.put(day, stateCounts);
 /*ALCODEEND*/}
 
--- ABMMaskEffectivenessV2/_alp/Agents/Person/Code/Events.java
+++ ABMMaskEffectivenessV2/_alp/Agents/Person/Code/Events.java
@@ -1,16 +1,7 @@
 void updateFearLevel()
 {/*ALCODESTART::1762151469016*/
-//System.out.println("Pop:" + main.startingPopulation);
-//System.out.println("Infective:" + main.People.Infective() + " | Dead: " + main.People.Dead());
-//System.out.println("Infective%: " + ((double) main.People.Infective() / main.startingPopulation));
-//System.out.println("Dead: " + ((double) main.People.Dead() / main.startingPopulation));
-
-//fearLevel = ((double) main.People.Infective() / (main.startingPopulation) + ((double) main.People.Dead() / (main.startingPopulation)));
-
-double fearRadius = main.spaceSideLength * 0.1; // radius of 10% of the box
-
 // Get list of neighbors
-List<Person> neighbors = getNeighbors(fearRadius);
+ArrayList<Person> neighbors = getSurroundingNeighbors();
 
 // Count how many neighbors are infective
 int nearbyInfective = 0;
@@ -20,40 +11,24 @@ for (Person p : neighbors) {
     }
 }
 
-// Count how many neighbors are dead
-int nearbyDead = 0;
-for (Person p : neighbors) {
-    if (p.isDead()) {
-        nearbyDead++;
-    }
+// Get the number of previous and current neighbors that have died
+for (int pIdx: neighborHistory) {
+	if (main.deadAgentIDs.contains(pIdx) && !deadNeighbors.contains(pIdx)) {
+		deadNeighbors.add(pIdx);	
+	}
 }
+
+
 
 // Get the total number of neighbors
 int neighborhoodSize = neighbors.size();
 
 // Calculate the fear level for this agent
 if (neighborhoodSize > 0) {
-    fearLevel = (((double) nearbyInfective / neighborhoodSize) + ((double) nearbyDead / neighborhoodSize));
+    fearLevel = (((double) nearbyInfective / neighborhoodSize) + (double) deadNeighbors.size() / neighborHistory.size());
 } else {
-    fearLevel = 0; // No neighbors no fear
-}
-/*ALCODEEND*/}
-
-void updateVaccinationRate()
-{/*ALCODESTART::1762152563464*/
-vaccinationRate = main.initialVaccinationRate - main.initialVaccineHesitancy + ((fearLevel * 100)*main.fearInducedVaccinationAcceptanceIncreaseRate);
-if (vaccineMandateAffected) {
-	vaccinationRate += main.vaccineMandateInducedAdoptionRateIncrease;
-}
-/*ALCODEEND*/}
-
-void updateMaskAdoptionRate()
-{/*ALCODESTART::1762217367295*/
-maskAdoptionRate = main.initialMaskAdoptionRate + ((fearLevel * 100) * main.fearInducedMaskAdoptionIncreaseRate);;
-maskAdoptionRate += ((main.maskPopularity * 100) * main.popularityInducedMaskAdoptionIncreaseRate);
-
-if (maskMandateAffected) {
-	maskAdoptionRate += main.vaccineMandateInducedAdoptionRateIncrease;
+	// If no neighbors, only add the number of previous neighbors that have died
+    fearLevel = (double) deadNeighbors.size() / neighborHistory.size();
 }
 /*ALCODEEND*/}
 
