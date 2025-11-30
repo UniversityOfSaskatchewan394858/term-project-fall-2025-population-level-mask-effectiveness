e808f2ae82c86b2b3da9e0e22d2eb6b8c69af476
Liam Oscaris <hwu126@usask.ca>
2025-11-29 19:01:16 -0600

    modify agent initialization
    - Agents can now be initialized such that specified fractions of the population begin in the Susceptible, Exposed, and Recovered states. This is done through the new parameters: percentageOfStartingExposed, percentageOfStartingInfective, percentageOfStartingRecovered

--- ABMMaskEffectivenessV2/_alp/Agents/Main/Code/Events.java
+++ ABMMaskEffectivenessV2/_alp/Agents/Main/Code/Events.java
@@ -3,13 +3,6 @@ void updateNetworkEvent()
 applyNetwork();
 /*ALCODEEND*/}
 
-void initialInfectionEvent()
-{/*ALCODESTART::1762017198552*/
-for (int i=0; i < initialInfectiveCount; i++) {
-	sendToRandomAgentInside("InitialInfect");
-}
-/*ALCODEEND*/}
-
 void checkInfectionNumber()
 {/*ALCODESTART::1762121326766*/
 traceln("Infected surpassed susceptibles on day: " + time());
